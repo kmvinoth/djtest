6c56bf087c9359f9b6fa3e812ffa2d6b99f47b13
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2016-12-13 11:49:30 +0100

    edited models.py

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     organization = models.CharField(max_length=100, default=True, blank=True)
     phone = models.CharField(max_length=20, default=True, blank=True)
+
+
+class Projects(models.Model):
+   project_name = models.CharField(max_length=100, default=True, blank=True)
