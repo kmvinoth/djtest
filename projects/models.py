6ccc0bf699cce62e54a64170bbb786a1a25bf884
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2017-01-10 15:06:00 +0100

    Add ProjectAdmin in admin.py (projects app)

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 
 class Project(models.Model):
     project_name = models.CharField(max_length=100)
-    admin = models.ForeignKey(User)
+    admin = models.ForeignKey(User, null=True, blank=True)
+
+    def __str__(self):
+        return self.project_name
 
     class Meta:
         verbose_name_plural = 'Project'
+
+
