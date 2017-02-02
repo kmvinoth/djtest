a3828a1681d3469c38ad187cc9a507f35fc769c7
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2017-02-02 11:43:35 +0100

    Made Project name read only

--- projects/forms.py
+++ projects/forms.py
@@ -3,6 +3,15 @@ from .models import Project, ProjectMemberRole
 
 
 class ProjectInfoForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        # Figure out how super works...!
+        super(ProjectInfoForm, self).__init__(*args, **kwargs)
+        # Read only field, so that project admin cannot edit
+        # Not foolproof because you can edit the Html and alter the value, during Post
+        # Ref : http://techstream.org/Bits/Read-only-field-in-django-ModelForm
+        self.fields['project_name'].widget.attrs['readonly'] = True
+
     class Meta:
         model = Project
         fields = ['project_name', 'admin', 'info']
