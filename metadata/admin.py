319b66ac76dbdc0865b3ca449011a47d221b5467
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2017-04-04 14:21:37 +0200

    Add dataobject model to admin

--- metadata/admin.py
+++ metadata/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import MetadataAttributes, Value, DepositValue
+from .models import MetadataAttributes, Value, DepositValue, DataobjectValue
 
 
 class MetadataAttributesAdmin(admin.ModelAdmin):
-    list_display = ['label', 'key', 'type']
+    list_display = ['label', 'key', 'type', 'meta_data_type', 'meta_data_level']
 
 
 class ValueAdmin(admin.ModelAdmin):
@@ -14,8 +14,13 @@ class DepositValueAdmin(admin.ModelAdmin):
     list_display = ['deposit', 'md_attributes', 'val']
 
 
+class DataobjectValueAdmin(admin.ModelAdmin):
+    list_display = ['dataobject', 'md_attributes', 'val']
+
+
 admin.site.register(MetadataAttributes, MetadataAttributesAdmin)
 admin.site.register(Value, ValueAdmin)
 admin.site.register(DepositValue, DepositValueAdmin)
+admin.site.register(DataobjectValue, DataobjectValueAdmin)
 
 
--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, ProjectMemberRole, Deposit
+from .models import Project, ProjectMemberRole, Deposit, DataObject
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -10,12 +10,17 @@ class DepositAdmin(admin.ModelAdmin):
     list_display = ['project', 'deposit_name']
 
 
+class DataobjectAdmin(admin.ModelAdmin):
+    list_display = ['deposit', 'data_object_name']
+
+
 class ProjectMemberRoleAdmin(admin.ModelAdmin):
     list_display = ['project', 'member', 'role']
 
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Deposit, DepositAdmin)
+admin.site.register(DataObject, DataobjectAdmin)
 admin.site.register(ProjectMemberRole, ProjectMemberRoleAdmin)
 
 
