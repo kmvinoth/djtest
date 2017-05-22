dfc502846fa79823724c99c6b4349fbced23327f
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2017-05-22 12:41:28 +0200

    Add docstrings for accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,10 @@
-""" djtest URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+This module contains the urls for accounts app. In future, add any Url related to accounts app here.
+
+The Views for login, password_reset, password_change, logout are used from the builtin Django auth views and the
+corresponding templates have been provided in the templates folder "accounts/templates/accounts"
+"""
+
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from accounts import views
