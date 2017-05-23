2cdbb1ec628bf72d5940d4fa2829619fe7a6ec95
Vinothkumar Mohanakrishnan <mohanakrishnan@zib.de>
2017-05-23 11:13:01 +0200

    Add docstrings for projects/forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,3 +1,13 @@
+"""
+This Module contains the Model Forms for the projects app.
+
+ProjectInfoForm = Used to add and edit info the project,
+ProjectMemberRoleForm = Used to assign & edit role to the project member,
+DepositForm = Used for entering meta data related to Deposit session,
+DataObjectForm = Used for entering metadata related to DataObject
+
+"""
+
 from django import forms
 from .models import Project, ProjectMemberRole, Deposit, DataObject
 
@@ -5,7 +15,6 @@ from .models import Project, ProjectMemberRole, Deposit, DataObject
 class ProjectInfoForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        # Figure out how super works...!
         super(ProjectInfoForm, self).__init__(*args, **kwargs)
         # Read only field, so that project admin cannot edit
         # Not foolproof because you can edit the Html and alter the value, during Post
@@ -18,6 +27,9 @@ class ProjectInfoForm(forms.ModelForm):
 
 
 class ProjectMemberRoleForm(forms.ModelForm):
+    # Make the Project field and the Member field Read only to the project admin,
+    # he can only edit the role of the memeber in the project
+    # Hint do it by altering the form __init__ method
     class Meta:
         model = ProjectMemberRole
         fields = ['project', 'member', 'role']
@@ -33,13 +45,3 @@ class DataobjectForm(forms.ModelForm):
     class Meta:
         model = DataObject
         fields = ['data_object_name']
-
-
-class ProjectMemberRoleEditForm(forms.ModelForm):
-
-    # Make the Project field and the Member field Read only to the project admin,
-    # he can only edit the role of the memeber in the project
-    # Hint do it by altering the form __init__ method
-    class Meta:
-        model = ProjectMemberRole
-        fields = ['project', 'member', 'role']
